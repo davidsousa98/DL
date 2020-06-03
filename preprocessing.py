e52bcc351ea3939f6cb1f2e87370929210af5278
David Sousa <55484746+davidsousa98@users.noreply.github.com>
2020-06-03 10:56:58 +0000

    kk

--- preprocessing.py
+++ preprocessing.py
@@ -3,6 +3,9 @@ import pandas as pd
 import numpy as np
 import zipfile as zp
 from sklearn.impute import KNNImputer
+import matplotlib.pyplot as plt
+import seaborn as sb
+from sklearn.model_selection import train_test_split
 
 def get_files_zip(zip):
     """
@@ -41,7 +44,7 @@ def append_files(files):
                     df = pd.read_excel(myfile)
                     list_files.append(df)
 
-    df_appened = pd.concat(list_files, axis =0)
+    df_appened = pd.concat(list_files, axis=0, sort=False)
     return df_appened.reset_index()
 
 
@@ -57,7 +60,7 @@ df_table_full = append_files(get_files_zip('league_table'))
 df_stats = df_stats_full[['Div','Date','HomeTeam','AwayTeam','FTHG','FTAG','HTHG','HTAG','HS','AS','HST','AST','HF',
                           'AF','HC','AC','HY','AY','HR','AR']]
 
-df_table = df_table_full[['Squad','GDiff', 'Pts', 'Season']]
+df_table = df_table_full[['Squad', 'MP','GDiff', 'Pts', 'Season']]
 
 ###########################################  MISSING VALUES TREATMENT  #################################################
 
@@ -119,10 +122,73 @@ filled_df.loc[filled_df["Div"] == "P1", "League"] = "Liga NOS"
 filled_df.drop(columns="Div", inplace=True)
 
 
+# Rationing variables
+df_table["points_per_game"] = df_table["Pts"] / df_table["MP"]
+df_table["goaldiff_per_game"] = df_table["GDiff"] / df_table["MP"]
+df_table.drop(columns=["Pts", "GDiff", "MP"], inplace=True)
+
+
 # Groupby
 df_grouped = df_stats.groupby('HomeTeam').count()
 
 
+########################################## OUTLIERS RECOGNITION ########################################################
+
+# Boxplot visualization
+f, axes = plt.subplots(2, 5, figsize=(4, 2), squeeze=False)
+sb.boxplot(df["First_Policy"], color="skyblue", ax=axes[0, 0])
+sb.boxplot(df["Birthday"], color="olive", ax=axes[0, 1])
+sb.boxplot(df["Salary"], color="gold", ax=axes[0, 2])
+sb.boxplot(df["CMV"], whis=5, color="green", ax=axes[0, 3])
+sb.boxplot(df["Claims"], color="yellow", ax=axes[0, 4])
+sb.boxplot(df["Motor"], color="orange", ax=axes[1, 0])
+sb.boxplot(df["Household"], whis=7, color="brown", ax=axes[1, 1])
+sb.boxplot(df["Health"], whis=2.5, color="lavender", ax=axes[1, 2])
+sb.boxplot(df["Life"], whis=7.5, color="greenyellow", ax=axes[1, 3])
+sb.boxplot(df["Work_Compensations"], whis=7, color="m", ax=axes[1, 4])
+
+# Histogram visualization
+f, axes = plt.subplots(2, 5, figsize=(7, 7))
+plt.subplots_adjust(wspace = 0.3, hspace = 0.3)
+sb.distplot(df["First_Policy"], color="skyblue", ax=axes[0, 0], kde=False)
+sb.distplot(df["Birthday"], color="olive", ax=axes[0, 1], kde=False)
+sb.distplot(df["Salary"], color="gold", ax=axes[0, 2], kde=False)
+sb.distplot(df["CMV"], color="green", ax=axes[0, 3], kde=False)
+sb.distplot(df["Claims"], color="yellow", ax=axes[0, 4], kde=False)
+sb.distplot(df["Motor"], color="orange", ax=axes[1, 0], kde=False)
+sb.distplot(df["Household"], color="brown", ax=axes[1, 1], kde=False)
+sb.distplot(df["Health"], color="lavender", ax=axes[1, 2], kde=False)
+sb.distplot(df["Life"], color="greenyellow", ax=axes[1, 3], kde=False)
+sb.distplot(df["Work_Compensations"], color="m", ax=axes[1, 4], kde=False)
+
+df['Outlier'] = 0
+df.loc[df['First_Policy'] > 2020, 'Outlier'] = 1
+
+df['Outlier'].value_counts()
+
+df = df.loc[df['Outlier'] == 0]
+
+# Correlation analysis
+plt.rcParams['figure.figsize'] = (12,12)
+
+corr_matrix=df.corr(method='????')
+mask = np.zeros_like(corr_matrix, dtype=np.bool)
+mask[np.triu_indices_from(mask)] = True
+sb.heatmap(data=corr_matrix, mask=mask, center=0, annot=True, linewidths=2, cmap='coolwarm')
+plt.tight_layout()
+
+
+
+variables = ['Severity', 'Age', 'Male', 'Santa Fe', 'Family_cases', 'Parents_infected_bin']
+
+# Data partition
+test_df = df.loc[df["Season"] == "2019/20"]
+
+# note: exclude season 19/20 from train/val
+X = df[variables]
+y = df['Deceased']
+
+X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.3, random_state=15, shuffle=True)
 
 
 
